29b5d318f9f7e2c575786f581c282e17a4e32bde
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-11-16 21:26:51 +0530

    updated content

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,13 @@ function Header (){
     <nav className="NavbarItems">
         <h1>Car Rental<i className=""></i></h1>
         <nav>
+            <ul>
             <li><a href="home">Home</a></li>
             <li><a href="home">About</a></li>
             <li><a href="home">Cars</a></li>
             <li><a href="home">Users</a></li>
             <li><a href="home">Contact</a></li>
+            </ul>
         </nav>
     </nav>
     )
---
+++ src/components/Header/MenuData.js
@@ -0,0 +1,32 @@
+export const MenuData =[
+{
+    title :"Home",
+    url : "#",
+    cName : "nav-links",
+    icon : "fa-solid fa-house-user"
+},
+{
+    title :"About",
+    url : "#",
+    cName : "nav-links",
+    icon : "fa-solid fa-house-user"
+},
+{
+    title :"Cars",
+    url : "#",
+    cName : "nav-links",
+    icon : "fa-solid fa-house-user"
+},
+{
+    title :"Users",
+    url : "#",
+    cName : "nav-links",
+    icon : "fa-solid fa-house-user"
+},
+{
+    title :"Contact",
+    url : "#",
+    cName : "nav-links",
+    icon : "fa-solid fa-house-user"
+},
+]
