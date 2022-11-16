e31ac64dd10b11e40e5903878c9de82c9637e7c6
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-11-16 23:58:42 +0530

    updated content

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,25 @@
 import React from "react"
 import { MenuData } from "./MenuData";
-import "../../styles/NavBar.css"
+import "../../styles/NavBar.css";
 
-function Header (){
+function Header() {
+    // state = {clicked:false}
     return (
-    <nav className="NavbarItems">
-        <h1 className="logo">Car Rental  <i className="fa-solid fa-car"></i><i className=""></i></h1>
-        <ul className="nav-menu">
-        {MenuData.map((item,index)=>{
-            return (  
-                <li key={index}><a href={item.url} className={item.cName}>
-                 <i className={item.icon}></i>{item.title}</a></li>
-            )
-        })}
-        </ul>
-    </nav>
+        <nav className="NavbarItems">
+            <h1 className="logo">Car Rental  <i className="fa-solid fa-car"></i><i className=""></i></h1>
+            {/* <div className="menu-icon">
+                <i className={this.state.clicked ? "fas fa-times": "fas fa-bars"}></i>
+            </div> */}
+            <ul className="nav-menu">
+                {MenuData.map((item, index) => {
+                    return (
+                        <li key={index}><a href={item.url} className={item.cName}>
+                            <i className={item.icon}></i>{item.title}</a></li>
+                    )
+                })}
+            </ul>
+            
+        </nav>
     )
 }
 
