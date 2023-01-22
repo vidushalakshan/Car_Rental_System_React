0dc5c95fccae5bf8327428a05a37ae5f0ff4dce9
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-01-22 12:23:48 +0530

    updated about desc

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import Home from "./components/pages/Home";
 
 import "../src/styles/normalize.css"
+import "../src/styles/background.css"
 import AboutUs from "./components/pages/AboutUs";
 
 
--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 import "../../styles/aboutUs.css";
+
+
 const AboutUs  = () => {
     return (
         <div>
             <h1 className="topic">About Us</h1>
+            <section>
+                <h2 className="welcome_service">Welcome to car rent service</h2>
+                <h3 className="about_desc">
+                    <pre>
+                        With over 30 years of experience in the industry, we strive
+                    </pre>
+                    <pre>
+                        to offer the highest levels of customer service and a highly 
+                    </pre>
+                    <pre>
+                        personalised service to all our customers who are on the lookout
+                    </pre>
+                    <pre>
+                        for Sri Lanka car rental opportunities. With one of the largest
+                    </pre>
+                    <pre>
+                        and most modern and varied fleets in Sri Lanka, our service is 
+                    </pre>
+                    <pre>
+                        backed by a networked front office, fully-fledged mechanical 
+                    </pre>
+                    <pre>
+                        servicing and valet servicing onsite.
+                    </pre>
+                </h3> 
+            </section>
         </div>
     );
 }
