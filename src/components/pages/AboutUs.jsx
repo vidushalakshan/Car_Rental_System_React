951fa67de5d5bde6ed42e6440338b5f35d8f0193
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-01-22 20:24:12 +0530

    updated aboutUs content

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -1,36 +1,33 @@
 import React from "react";
+import Grid from '@mui/material/Grid';
 import "../../styles/aboutUs.css";
+import { Box } from "@mui/system";
+import Font, { Text } from 'react-font'
 
 
 const AboutUs  = () => {
+    
     return (
+        
         <div>
+            <Font family='Viga'>
             <h1 className="topic">About Us</h1>
+            </Font>
             <section>
+                <Font family='Viga'>
                 <h2 className="welcome_service">Welcome to car rent service</h2>
-                <h3 className="about_desc">
-                    <pre>
-                        With over 30 years of experience in the industry, we strive
-                    </pre>
-                    <pre>
-                        to offer the highest levels of customer service and a highly 
-                    </pre>
-                    <pre>
-                        personalised service to all our customers who are on the lookout
-                    </pre>
-                    <pre>
-                        for Sri Lanka car rental opportunities. With one of the largest
-                    </pre>
-                    <pre>
-                        and most modern and varied fleets in Sri Lanka, our service is 
-                    </pre>
-                    <pre>
-                        backed by a networked front office, fully-fledged mechanical 
-                    </pre>
-                    <pre>
-                        servicing and valet servicing onsite.
-                    </pre>
-                </h3> 
+                </Font>
+            </section>
+            <section className="about_desc">
+                <Box sx={{ flexGrow: 1 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={4}>
+                        <item className="desc">
+                        With over 30 years of experience in the industry, we strive to offer the highest levels of customer service and a highly personalised service to all our customers who are on the lookout for Sri Lanka car rental opportunities. With one of the largest and most modern and varied fleets in Sri Lanka, our service is backed by a networked front office, fully-fledged mechanical servicing and valet servicing onsite. 
+                        </item>
+                        </Grid>
+                    </Grid>
+                </Box>
             </section>
         </div>
     );
