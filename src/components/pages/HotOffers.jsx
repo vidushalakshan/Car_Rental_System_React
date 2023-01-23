ea2efd7fee9ecbedf2a1617180c6714a69544cec
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-01-24 01:08:30 +0530

    updated hot offers content

--- src/components/pages/HotOffers.jsx
+++ src/components/pages/HotOffers.jsx
@@ -1,11 +1,56 @@
 import React from "react";
 import "../../styles/hotOffers.css"
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 const HotOffers  = () => {
     return(
         <div>
-            <h3 className="smTitle">Come with</h3>
-            <h1 className="bigTitle">Hot Offers</h1>
+            <div>
+                <h3 className="smTitle">Come with</h3>
+                <h1 className="bigTitle">Hot Offers</h1>
+            </div>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={3}>
+                    <Grid item xs={4}>
+                        <Item>
+                            <div className="car_item">
+                                <div >
+                                    <img className="car_img" src="https://s1.1zoom.me/big0/660/Shelby_Super_Cars_White_background_White_559662_1280x853.jpg"></img>
+                                </div>
+                            </div>
+                        </Item>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Item>
+                            <div className="car_item">
+                                <div >
+                                    <img className="car_img" src="https://s1.1zoom.me/big0/660/Shelby_Super_Cars_White_background_White_559662_1280x853.jpg"></img>
+                                </div>
+                            </div>
+                        </Item>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Item>
+                            <div className="car_item">
+                                <div >
+                                    <img className="car_img" src="https://s1.1zoom.me/big0/660/Shelby_Super_Cars_White_background_White_559662_1280x853.jpg"></img>
+                                </div>
+                            </div>
+                        </Item>
+                    </Grid>
+                </Grid>
+            </Box>
         </div>
     )
 }
