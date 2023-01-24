e71f55f61532de754d25efac9c038d0e8dbcb7fe
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-01-24 16:09:40 +0530

    updated hotOffers content

--- src/components/pages/HotOffers.jsx
+++ src/components/pages/HotOffers.jsx
@@ -116,6 +116,100 @@ const HotOffers  = () => {
                     </Grid>
                 </Grid>
             </Box>
+
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={3}>
+                    <Grid item xs={4}>
+                        <Item>
+                            <div className="car_item">
+                                <div >
+                                    <img className="car_img" src="https://cdn.jarviscars.com.au//img/news/toyota/toyota-prado.jpg"></img>
+                                </div>
+
+                                <div>
+                                    <h4 className="section_title">Toyota Aventador</h4>
+                                    <h6 className="priceDate">$40.00<span>/ Day</span></h6>
+                                </div>
+
+                                <div>
+                                     <span className="model">
+                                     A/C, Power Steering/CD Player,Manual
+                                    </span>
+                                </div>
+                                <Stack direction="row">
+                                    <div className="stack">
+                                    <Button variant="contained" className="btn1">
+                                        Rent
+                                    </Button>
+                                    <Button variant="contained" color="success" className="btn1">
+                                        Details
+                                    </Button>
+                                    </div>
+                                </Stack>
+                            </div>
+                        </Item>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Item>
+                            <div className="car_item">
+                                <div >
+                                    <img className="car_img" src="https://us.123rf.com/450wm/whitecity/whitecity2107/whitecity210700522/whitecity210700522.jpg?ver=6"></img>
+                                </div>
+
+                                <div>
+                                    <h4 className="section_title">Toyota Aventador</h4>
+                                    <h6 className="priceDate">$40.00<span>/ Day</span></h6>
+                                </div>
+
+                                <div>
+                                     <span className="model">
+                                     A/C, Power Steering/CD Player,Manual
+                                    </span>
+                                </div>
+                                <Stack direction="row">
+                                    <div className="stack">
+                                    <Button variant="contained" className="btn1">
+                                        Rent
+                                    </Button>
+                                    <Button variant="contained" color="success" className="btn1">
+                                        Details
+                                    </Button>
+                                    </div>
+                                </Stack>
+                            </div>
+                        </Item>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Item>
+                            <div className="car_item">
+                                <div >
+                                    <img className="car_img" src="https://s3.us-east-2.amazonaws.com/dealer-inspire-vps-vehicle-images/11000623/JTDKAMFU4N3188731/dffef90d727480b574f62001f5d79f85.jpg"></img>
+                                </div>
+                                <div>
+                                    <h4 className="section_title">Toyota Aventador</h4>
+                                    <h6 className="priceDate">$40.00<span>/ Day</span></h6>
+                                </div>
+
+                                <div>
+                                     <span className="model">
+                                     A/C, Power Steering/CD Player,Manual
+                                    </span>
+                                </div>
+                                <Stack direction="row">
+                                    <div className="stack">
+                                    <Button variant="contained" className="btn1">
+                                        Rent
+                                    </Button>
+                                    <Button variant="contained" color="success" className="btn1">
+                                        Details
+                                    </Button>
+                                    </div>
+                                </Stack>
+                            </div>
+                        </Item>
+                    </Grid>
+                </Grid>
+            </Box>
         </div>
     )
 }
