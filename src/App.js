1ae33572219785226a86f6d977d0f64afa899576
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-01-26 16:43:31 +0530

    aded dashboard content and update some content

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ import "../src/styles/background.css";
 import AboutUs from "./components/pages/AboutUs";
 import HotOffers from "./components/pages/HotOffers";
 import Contact from "./components/common/contact/Contact";
+import DashBoard from "./admin_pages/DashBoard";
+
 
 function App() {
+
+  const isBackgroundRed = true;
+  
   return (
     <div>
-      <Header />
-      <Home/>
-      <AboutUs/>
-      <HotOffers/>
-      <Contact/>
+      <DashBoard/>
     </div>
   );
 }
--- src/admin_pages/DashBoard.jsx
+++ src/admin_pages/DashBoard.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 const DashBoard = () => {
     return (
-        <div>
-            
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={3}>
+                    <Grid item xs={4}>
+                        <Item>
+                            <div>
+                                <h2>Admin Dashboard</h2>
+                                <hr></hr>
+                                <div>
+                                    
+                                </div>
+                            </div>
+                        </Item>
+                    </Grid>
+                </Grid>
+            </Box>    
     )
 }
 
