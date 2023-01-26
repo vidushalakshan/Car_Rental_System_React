37813c104f10425de6fe234faf2e5709d30165bb
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-01-26 18:50:32 +0530

    aded icon dashboard

--- src/admin_pages/DashBoard.jsx
+++ src/admin_pages/DashBoard.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
-import "../../src/styles/dashBoard.css"
+import "../../src/styles/dashBoard.css";
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import AirlineSeatReclineExtraIcon from '@mui/icons-material/AirlineSeatReclineExtra';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import CarRentalIcon from '@mui/icons-material/CarRental';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import PaymentsIcon from '@mui/icons-material/Payments';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -17,35 +23,36 @@ const Item = styled(Paper)(({ theme }) => ({
 
 const DashBoard = () => {
     return (
+        
         <Box sx={{ flexGrow: 1 }}>
                 <Grid container spacing={3}>
-                    <Grid item xs={4}>
+                    <Grid item xs={3}>
                         <Item>
-                            <div>
+                            <div className="dash_page">
                                 <h2>Admin Dashboard</h2>
                                 <hr></hr>
-                                <div>
+                                <div className="page_link">
                                     <ul className="dash_btn">
-                                        <li>
-                                            <Button>Dashboard</Button>
+                                        <li className="page">
+                                            <Button className="icon1"><DashboardIcon></DashboardIcon> Dashboard</Button>
                                         </li>
-                                        <li>
-                                            <Button>Admin</Button>
+                                        <li className="page">
+                                            <Button><AdminPanelSettingsIcon></AdminPanelSettingsIcon>Admin</Button>
                                         </li>
-                                        <li>
-                                            <Button>Drivers</Button>
+                                        <li className="page">
+                                            <Button><AirlineSeatReclineExtraIcon></AirlineSeatReclineExtraIcon>Drivers</Button>
                                         </li>
-                                        <li>
-                                            <Button>Customers</Button>
+                                        <li className="page">
+                                            <Button><AccountCircleIcon></AccountCircleIcon>Customers</Button>
                                         </li>
-                                        <li>
-                                            <Button>Cars</Button>
+                                        <li className="page">
+                                            <Button><CarRentalIcon></CarRentalIcon>Cars</Button>
                                         </li>
-                                        <li>
-                                            <Button>Booking</Button>
+                                        <li className="page">
+                                            <Button><LibraryBooksIcon></LibraryBooksIcon>Booking</Button>
                                         </li>
-                                        <li>
-                                            <Button>Payment</Button>
+                                        <li className="page">
+                                            <Button><PaymentsIcon></PaymentsIcon>Payment</Button>
                                         </li>
                                     </ul>
                                 </div>
@@ -54,6 +61,7 @@ const DashBoard = () => {
                     </Grid>
                 </Grid>
             </Box>    
+            
     )
 }
 
