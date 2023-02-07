a38441547152c358501e992e048a2edf58611672
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-02-07 23:38:58 +0530

    updated textField and button cars content

--- src/admin_pages/Cars.jsx
+++ src/admin_pages/Cars.jsx
@@ -1,13 +1,118 @@
 import React from "react";
 import "../styles/admin/car.css";
-
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 
 const Cars = () => {
     return (
-        <div>
-
+        <Box
+        component="form"
+        sx={{
+          "& .MuiTextField-root": { m: 3, width: "35ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        {/* Start textField content */}
+  
+        <div className="txtAdmin">
+          <TextField
+            id="standard-textarea"
+            label="Driver_ID"
+            multiline
+            variant="standard"
+          />
+          <TextField
+            id="standard-textarea"
+            label="Full_Name"
+            multiline
+            variant="standard"
+          />
+          <TextField
+            id="standard-textarea"
+            label="Address"
+            multiline
+            variant="standard"
+          />
+          <TextField
+            id="standard-textarea"
+            label="Mobile_Number"
+            multiline
+            variant="standard"
+          />
+          <TextField
+            id="standard-textarea"
+            label="Password"
+            multiline
+            variant="standard"
+          />
+          <TextField
+            id="standard-textarea"
+            label="Email"
+            multiline
+            variant="standard"
+          />
+          <TextField
+            id="standard-textarea"
+            label="Email"
+            multiline
+            variant="standard"
+          />
+          <TextField
+            id="standard-textarea"
+            label="Email"
+            multiline
+            variant="standard"
+          />
+          <TextField
+            id="standard-textarea"
+            label="Email"
+            multiline
+            variant="standard"
+          />
+          <TextField
+            id="standard-textarea"
+            label="Email"
+            multiline
+            variant="standard"
+          />
+          <TextField
+            id="standard-textarea"
+            label="Email"
+            multiline
+            variant="standard"
+          />
         </div>
+  
+        {/* End textField content */}
+  
+        {/* Start button content */}
+  
+        <div className="btnCars">
+          <Stack direction="row" spacing={2}>
+            <Button variant="contained" color="primary">
+              Save
+            </Button>
+            <Button variant="contained" color="success">
+              Update
+            </Button>
+            <Button variant="contained" color="error">
+              Delete
+            </Button>
+            <Button variant="contained" color="secondary">
+              Clear
+            </Button>
+          </Stack>
+        </div>
+  
+        {/* End button content */}
+  
+        {/* Start table content */}
+  
+      </Box>
     )
 };
 
--- src/data/script.jsx
+++ src/data/script.jsx
@@ -1,36 +1,33 @@
-import $ from 'jquery';
+import $ from "jquery";
 
-$("#admin").css("display", "none");
+$("#dash").css("display", "none");
 $("#driver").css("display", "none");
 $("#cars").css("display", "none");
 
-
 $("#adminLink").click(function () {
-    $("#dash").css("display", "none");
-    $("#admin").css("display", "block");
-    $("#driver").css("display","none");
-    $("#cars").css("display","none");
+  $("#dash").css("display", "none");
+  $("#admin").css("display", "block");
+  $("#driver").css("display", "none");
+  $("#cars").css("display", "none");
 });
 
 $("#dashLink").click(function () {
-    $("#dash").css("display", "block");
-    $("#admin").css("display", "none");
-    $("#driver").css("display","none");
-    $("#cars").css("display","none");
+  $("#dash").css("display", "block");
+  $("#admin").css("display", "none");
+  $("#driver").css("display", "none");
+  $("#cars").css("display", "none");
 });
 
-$("#driverLink").click(function(){
-    $("#dash").css("display", "none");
-    $("#admin").css("display", "none");
-    $("#driver").css("display","block");
-    $("#cars").css("display","none");
+$("#driverLink").click(function () {
+  $("#dash").css("display", "none");
+  $("#admin").css("display", "none");
+  $("#driver").css("display", "block");
+  $("#cars").css("display", "none");
 });
 
-$("#carLink").click(function(){
-    $("#dash").css("display", "none");
-    $("#admin").css("display", "none");
-    $("#driver").css("display","none");
-    $("#cars").css("display","block");
+$("#carLink").click(function () {
+  $("#dash").css("display", "none");
+  $("#admin").css("display", "none");
+  $("#driver").css("display", "none");
+  $("#cars").css("display", "block");
 });
-
-
