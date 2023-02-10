b5f6bd035657aa43236c0893f1323eb3963263d4
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-02-10 22:24:07 +0530

    updated content

--- src/components/pages/PaymentDetails.jsx
+++ src/components/pages/PaymentDetails.jsx
@@ -4,6 +4,7 @@ import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
+import "../../styles/payment.css"
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -26,7 +27,7 @@ const PaymentDetails = () => {
       >
         <Grid container spacing={5}>
           <Grid item xs={6}>
-            <Item>
+            <Item className="customer_content">
               <TextField
                 id="standard-textarea"
                 label="Full_Name"
