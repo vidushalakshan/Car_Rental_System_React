d3d25a0a8368f4d6bb85b1dbba0dff4bca3e97b4
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-02-11 18:02:04 +0530

    updated content

--- src/components/pages/PaymentDetails.jsx
+++ src/components/pages/PaymentDetails.jsx
@@ -9,6 +9,13 @@ import DatePicker from "react-date-picker";
 import TimePicker from "react-time-picker";
 import Contact from "../common/contact/Contact";
 import { color } from "@mui/system";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -74,14 +81,15 @@ const PaymentDetails = () => {
 
           <Grid item xs={6}>
             <div className="info">
-                <h2 className="title_info">Great choice
-                </h2>
-                <h4 className="details">*Additional driver include (worth $60)</h4>
-                <h4 className="details">*Most popular fuel policy</h4>
-                <h4 className="details">*Easy to find counter</h4>
-                <h4 className="details">*Customer rating: 8.9 / 10</h4>
-                <h4 className="details">*Short lines(worth $60)</h4>
-                <h4 className="details">*Free Cancellation(worth $60)</h4>
+              <h2 className="title_info">Great choice</h2>
+              <h4 className="details">
+                *Additional driver include (worth $60)
+              </h4>
+              <h4 className="details">*Most popular fuel policy</h4>
+              <h4 className="details">*Easy to find counter</h4>
+              <h4 className="details">*Customer rating: 8.9 / 10</h4>
+              <h4 className="details">*Short lines(worth $60)</h4>
+              <h4 className="details">*Free Cancellation(worth $60)</h4>
             </div>
             <Item className="payment_info">
               <h2>Payment Information</h2>
@@ -110,6 +118,29 @@ const PaymentDetails = () => {
                   className="date"
                 />
               </div>
+              <FormControl className="redio">
+                <RadioGroup
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  defaultValue="female"
+                  name="radio-buttons-group"
+                >
+                  <FormControlLabel
+                    value="female"
+                    control={<Radio />}
+                    label="Direct Band Transfer"
+                  />
+                  <FormControlLabel
+                    value="male"
+                    control={<Radio />}
+                    label="Master Card"
+                  />
+                  <FormControlLabel
+                    value="other"
+                    control={<Radio />}
+                    label="Paypal"
+                  />
+                </RadioGroup>
+              </FormControl>
             </Item>
           </Grid>
           <Grid item xs={12}>
