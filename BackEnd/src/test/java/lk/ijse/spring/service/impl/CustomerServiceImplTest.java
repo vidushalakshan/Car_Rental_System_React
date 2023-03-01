f0a9ef4038747f7b82d43843ce53e531281c84e8
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-03-02 03:21:37 +0530

    updated

---
+++ BackEnd/src/test/java/lk/ijse/spring/service/impl/CustomerServiceImplTest.java
@@ -0,0 +1,39 @@
+package lk.ijse.spring.service.impl;
+
+import lk.ijse.spring.config.WebAppConfig;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+@WebAppConfiguration
+@ContextConfiguration(classes = {WebAppConfig.class})
+@ExtendWith(SpringExtension.class)
+class CustomerServiceImplTest {
+
+    @Test
+    void saveCustomer() {
+    }
+
+    @Test
+    void deleteCustomer() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+
+    @Test
+    void searchCustomer() {
+    }
+
+    @Test
+    void getAllCustomers() {
+    }
+
+    @Test
+    void generateCustomerId() {
+    }
+}
