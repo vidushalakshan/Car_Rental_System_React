d6f425ed3f7cac3973f73acb89864b720541fb37
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2023-03-02 02:36:20 +0530

    updated save adminServiceImpl content

--- BackEnd/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/impl/AdminServiceImpl.java
@@ -1,4 +1,53 @@
 package lk.ijse.spring.service.impl;
 
-public class AdminServiceImpl {
+import lk.ijse.spring.dto.AdminDTO;
+import lk.ijse.spring.entity.Admin;
+import lk.ijse.spring.repo.AdminRepo;
+import lk.ijse.spring.service.AdminService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class AdminServiceImpl implements AdminService {
+
+    @Autowired
+    private AdminRepo adminRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public void saveAdmin(AdminDTO adminDTO) {
+        if (!adminRepo.existsById(adminDTO.getAdmin_Id())){
+            adminRepo.save(modelMapper.map(adminDTO, Admin.class));
+        }else {
+            throw new RuntimeException("Admin Already Exist..");
+        }
+    }
+
+    @Override
+    public void deleteAdmin(String adminId) {
+
+    }
+
+    @Override
+    public void updateAdmin(AdminDTO adminDTO) {
+
+    }
+
+    @Override
+    public AdminDTO searchAdmin(String id) {
+        return null;
+    }
+
+    @Override
+    public List<AdminDTO> getAllAdmin() {
+        return null;
+    }
+
+    @Override
+    public String generateAdminId() {
+        return null;
+    }
 }
